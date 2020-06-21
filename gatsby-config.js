7350c102718f99fea1434e06befec0193c10b156
Charmander <~@charmander.me>
2020-06-21 02:27:54 -0700

    Use gatsby-remark-autolink-headers to get improved heading ids
    Fixes formatted text (e.g. inline code) being rendered as “[object Object]” (#84), and multiple text children (e.g. `new Pool([config: object])`, for some reason) being rendered with commas. Also now compatible with GitHub!
    
    Can be used to add link icons beside each heading later by removing `icon: false` from its configuration (but needs additional CSS).

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,12 @@ module.exports = {
       options: {
         gatsbyRemarkPlugins: [
           {
+            resolve: 'gatsby-remark-autolink-headers',
+            options: {
+              icon: false
+            }
+          },
+          {
             resolve: "gatsby-remark-images",
             options: {
               maxWidth: 1035,
